abcdb2875c7c56bb7c9fc4929667c8da812278e2
Nicolas Sanchez <sanchez.nicolas.9917@gmail.com>
2025-05-06 14:20:18 -0400

    base

---
+++ Assets/_Scripts/Target.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
